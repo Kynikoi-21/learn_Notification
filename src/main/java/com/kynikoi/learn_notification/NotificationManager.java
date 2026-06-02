a1da98f656207a045d09d4f2f3e3cfc936e52024
Kynikoi <kynikoi2004@gmail.com>
2026-06-02 23:34:07 +0800

    learn for dependency inject

---
+++ src/main/java/com/kynikoi/learn_notification/NotificationManager.java
@@ -0,0 +1,22 @@
+package com.kynikoi.learn_notification;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationManager {
+
+    private final NotificationService notificationService;
+
+
+    public NotificationManager(@Qualifier("SMS") NotificationService notificationService){
+        this.notificationService = notificationService;
+
+    }
+
+    public void sendNotification(String message){
+        notificationService.send(message);
+    }
+
+}
