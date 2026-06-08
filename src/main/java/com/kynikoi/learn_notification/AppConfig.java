925598b1ced1b3b5f89ae343e37ae705e55bfaab
Kynikoi <kynikoi2004@gmail.com>
2026-06-08 23:35:57 +0800

    练习@PostConstruct和@PreDestroy，PostConstruct用来在bean对象创建后，构造函数跑完后被调用，来进行一些配置；PreDestroy在对象销毁前执行，进行一些清理

--- src/main/java/com/kynikoi/learn_notification/AppConfig.java
+++ src/main/java/com/kynikoi/learn_notification/AppConfig.java
@@ -1,5 +1,7 @@
 package com.kynikoi.learn_notification;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -12,8 +14,15 @@ public class AppConfig {
     public EmailNotificationService Email(){
         return new EmailNotificationService();
     }
+    @PostConstruct
+    public void init(){
+        System.out.println("PostConstruct already created");
+    }
+    @PreDestroy
+    public void cleanup(){
+        System.out.println("NotificationManager Predestroy");
+    }
 
-    @Scope("prototype")
     @Bean
     public NotificationManager notificationManager(){
         return new NotificationManager(Email());
--- src/main/java/com/kynikoi/learn_notification/LearnNotificationApplication.java
+++ src/main/java/com/kynikoi/learn_notification/LearnNotificationApplication.java
@@ -3,16 +3,18 @@ package com.kynikoi.learn_notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class LearnNotificationApplication {
 
     public static void main(String[] args) {
 
-        ApplicationContext context= SpringApplication.run(LearnNotificationApplication.class, args);
+        ConfigurableApplicationContext context= SpringApplication.run(LearnNotificationApplication.class, args);
         var notificationManager = context.getBean(NotificationManager.class);
         var notificationManager2 = context.getBean(NotificationManager.class);
         notificationManager2.sendNotification("EmailNotificationService");
+        context.close();
     }
 
 
